4270eef6ed0b126f37d27e5df3e9cb68b8ff4cdf
Marcelina Lima <marcelina.batista@hotmail.com>
2023-01-23 18:54:59 -0300

    Atualizando informaçoes nos arquivos

--- variaveis.js
+++ variaveis.js
@@ -6,16 +6,16 @@
 
 //---------------------------------------
 /*
- declarando variavel com var, let e const
+ declarando variável com var, let e const
  */
-var nome_completo="joao da silva";//variavel pode ser alterada
-let idade = 20; // variavel pode ser alterada
-const cpf = "123456798"; // variavel nao pode ser alterada
+var nome_completo="joão da silva";//variável pode ser alterada
+let idade = 20; // variável pode ser alterada
+const cpf = "123456798"; // variável nao pode ser alterada
 
 //console.log serve para "printar/mostrar" o valor no console do navegador
 console. log(nome_completo, idade,cpf)
 
-//alterar o valor das variaveis
+//alterar o valor das variáveis
 
 nome_completo = "Alice";
 idade = 20;
